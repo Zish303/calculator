9a5819ef2df130af0747896c7e8f3374d34b848e
Zish303 <103155461+Zish303@users.noreply.github.com>
2022-08-13 23:41:01 +0530

    update

--- index.js
+++ index.js
@@ -1,168 +1,139 @@
-const ans = document.getElementById("ans")
-// ans.value = 11
-const val = document.getElementById("val")
-// input.addEventListener("keypress", function (event) {
-//     if (event.key === "Enter") {
-//         event.preventDefault();
-//         document.getElementById("+").click();
-//     }
-// });
+const ans = document.getElementById("input")
 
-temp = ""
-fir = ""
-sec = ""
 function on9() {
-    if (temp != "") sec += 9
-    else fir += 9
-    val.value += 9
-    // ans.value += 9
+    ans.textContent += 9
 }
 function on8() {
-    if (temp != "") sec += 8
-    else fir += 8
-    val.value += 8
-    // ans.value += 8
+    ans.textContent += 8
 }
 function on7() {
-    if (temp != "") sec += 7
-    else fir += 7
-    val.value += 7
-    // ans.value += 7
+    ans.textContent += 7
 }
 function on6() {
-    if (temp != "") sec += 6
-    else fir += 6
-    val.value += 6
-    // ans.value += 6
+    ans.textContent += 6
 }
 function on5() {
-    if (temp != "") sec += 5
-    else fir += 5
-    val.value += 5
-    // ans.value += 5
+    ans.textContent += 5
 }
 function on4() {
-    if (temp != "") sec += 4
-    else fir += 4
-    val.value += 4
-    // ans.value += 4
+    ans.textContent += 4
 }
 function on3() {
-    if (temp != "") sec += 3
-    else fir += 3
-    val.value += 3
-    // ans.value += 3
+    ans.textContent += 3
 }
 function on2() {
-    if (temp != "") sec += 2
-    else fir += 2
-    val.value += 2
-    // ans.value += 2
+    ans.textContent += 2
 }
 function on1() {
-    if (temp != "") sec += 1
-    else fir += 1
-    val.value += 1
-    // ans.value += 1
+    ans.textContent += 1
 }
 function on0() {
-    if (temp != "") sec += 0
-    else fir += 0
-    val.value += 0
-    // ans.value += 0
+    ans.textContent += 0
 }
-function onC() {
-    val.value = ""
-    fir = ""
-    sec = ""
-    temp = ""
+function onper() {
+    ans.textContent += '.'
 }
 function onAC() {
-    ans.value = ""
-    onC()
+    ans.textContent = ""
 }
-function calc(temp) {
-    console.log("calc called")
-    if (temp === "+") {
-        val.value = parseInt(fir) + parseInt(sec)
-    }
-    if (temp === "-") {
-        val.value = parseInt(fir) - parseInt(sec)
-    }
-    if (temp === "*") {
-        val.value = parseInt(fir) * parseInt(sec)
-    }
-    else if (temp === "/") {
-        val.value = parseInt(fir) / parseInt(sec)
-    }
 
-    fir = val.value
-    sec = ""
-    ans.value = val.value
-}
-const onplus = document.getElementById("+");
-// onpl.onKeyPress = {
-//     (ev) => {
-//     if (ev.key === "Enter") {
-//         onpl()
-//     }
-// }
-// }
-onplus.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        onplus.click();
-    }
-})
 function onpl() {
-    if (temp != "") {
-        calc(temp)
-    }
-    else
-        ans.value = ""
-    val.value += "+"
-    temp = "+"
+    if (ans.textContent === "") return
+    let temp = ans.textContent
+    temp = eval(temp)
+    temp += "+"
+    ans.textContent = temp
 }
+
+function onpl() {
+    if (ans.textContent === "") return
+    let temp = ans.textContent
+    temp = eval(temp)
+    temp += "+"
+    ans.textContent = temp
+}
+
 function onmin() {
-    if (temp != "") {
-        calc(temp)
-    }
-    else
-        ans.value = ""
-    val.value += "-"
-    temp = "-"
+    if (ans.textContent === "") return
+    let temp = ans.textContent
+    temp = eval(temp)
+    temp += "-"
+    ans.textContent = temp
 }
+
 function onmul() {
-    if (temp != "") {
-        calc(temp)
-    }
-    else
-        ans.value = ""
-    val.value += "*"
-    temp = "*"
+    if (ans.textContent === "") return
+    let temp = ans.textContent
+    temp = eval(temp)
+    temp += "*"
+    ans.textContent = temp
 }
+
 function ondiv() {
-    if (temp != "") {
-        calc(temp)
-    }
-    else
-        ans.value = ""
-    val.value += "/"
-    temp = "/"
+    if (ans.textContent === "") return
+    let temp = ans.textContent
+    temp = eval(temp)
+    temp += "/"
+    ans.textContent = temp
 }
+
+function onmod() {
+    if (ans.textContent === "") return
+    let temp = ans.textContent
+    temp = eval(temp)
+    temp += "%"
+    ans.textContent = temp
+}
+
 function oneq() {
-    calc(temp)
-    val.value = ""
-    temp = ""
+    if (ans.textContent === "") return
+    let temp = ans.textContent
+    temp = eval(temp)
+    ans.textContent = temp
 }
-document.onkeypress = function (e) {
-    if (e.which == 43) {
-        onpl();
-    } else if (e.ctrlKey && e.which == 67) {
-        alert("Ctrl + B shortcut combination was pressed");
-    } else if (e.ctrlKey && e.altKey && e.which == 89) {
-        alert("Ctrl + Alt + Y shortcut combination was pressed");
-    } else if (e.ctrlKey && e.altKey && e.shiftKey && e.which == 85) {
-        alert("Ctrl + Alt + Shift + U shortcut combination was pressed");
+
+function ondel() {
+    let temp = ans.textContent
+    temp = temp.slice(0, -1)
+    ans.textContent = temp
+}
+
+document.onkeydown = (event) => {
+    if (event.key == "1")
+        on1()
+    if (event.key == "2")
+        on2()
+    if (event.key == "3")
+        on3()
+    if (event.key == "4")
+        on4()
+    if (event.key == "5")
+        on5()
+    if (event.key == "6")
+        on6()
+    if (event.key == "7")
+        on7()
+    if (event.key == "8")
+        on8()
+    if (event.key == "9")
+        on9()
+    if (event.key == "0")
+        on0()
+    if (event.key == ".")
+        onper()
+    if (event.key == "+")
+        onpl()
+    if (event.key == "-")
+        onmin()
+    if (event.key == "*")
+        onmul()
+    if (event.key == "/")
+        ondiv()
+    if (event.key == "Escape")
+        onAC()
+    if (event.key == "Enter")
+        oneq()
+    if (event.key == "Backspace") {
+        ondel()
     }
-};
+}
